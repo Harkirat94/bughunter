5be959c97a0fc362cd7e6f0621b8cda3ac582f8d
Harkirat <harkirat12039@iiitd.ac.in>
2018-10-17 02:10:47 -0400

    remove comments from sample file

--- src/sample/DSample1.java
+++ src/sample/DSample1.java
@@ -3,33 +3,14 @@ package sample;
 public class DSample1 {
 
 	public static int div(int a, int b){
-		/*int c;
-		if(b!=0){
-			c=a/b;
-		}
-		else{
-			c=a/b;
-		}
-		return c;*/
 		while(b!=0){
 			b=b-1;
 		}
 		return 0;
 	}
 	
-
-	/*public static int evil(int a, int b){
-		int c;
-		if(b!=0){
-			c=a+b;
-		}
-		c=a/b;
-		return c;
-	}*/
-
 	public static void main(String[] args) {
 		div(2,0);
-		//evil(2,0);
 	}
 
 }
